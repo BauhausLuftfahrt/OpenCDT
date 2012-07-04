f797a9d6440cd4c54ca5be3fb63314bbfc866948
Martin Glas <martin.glas@bauhaus-luftfahrt.net>
2012-07-04 16:05:44 +0000

    moved net.bhl.cdt.architecturaltools.model to archive
    git-svn-id: https://svn.code.sf.net/p/opencdt/svn/branches/archive@41 1b14dba9-cd1e-4c95-8206-bb1ba8f2ccbb

---
+++ net.bhl.cdt.architecturetools.model/src/net/bhl/cdt/architecturetools/exceptions/InterfaceHasNoParentComponentException.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * <copyright> Copyright (c) 2009-2012 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ *  materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ ******************************************************************************/
+/**
+ * 
+ */
+package net.bhl.cdt.architecturetools.exceptions;
+
+/**
+ * @author sven.ziemer
+ * 
+ */
+public class InterfaceHasNoParentComponentException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5949328501262582660L;
+
+}
