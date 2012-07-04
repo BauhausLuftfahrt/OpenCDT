da2fc0c8fbe8e009aa9aa9d3404d5d31515cc5d0
Martin Glas <martin.glas@bauhaus-luftfahrt.net>
2012-07-04 16:06:56 +0000

    moved net.bhl.cdt.architecturaltools.model.edit to archive
    git-svn-id: https://svn.code.sf.net/p/opencdt/svn/branches/archive@42 1b14dba9-cd1e-4c95-8206-bb1ba8f2ccbb

---
+++ net.bhl.cdt.architecturetools.model.edit/src/net/bhl/cdt/architecturetools/model/architecturetools/provider/ArchitecutremodelEditPlugin.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * <copyright> Copyright (c) 2009-2012 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ *  materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ ******************************************************************************/
+package net.bhl.cdt.architecturetools.model.architecturetools.provider;
+
+import net.bhl.cdt.model.provider.ModelEditPlugin;
+import net.bhl.cdt.utilities.basecalculationmodel.provider.BasecalculationmodelEditPlugin;
+import net.bhl.cdt.utilities.datatypes.provider.DatatypesEditPlugin;
+import net.bhl.cdt.utilities.exchangemodel.provider.ExchangemodelEditPlugin;
+import net.bhl.cdt.utilities.units.provider.UnitsEditPlugin;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+/**
+ * This is the central singleton for the Architecutremodel edit plugin. <!--
+ * begin-user-doc --> <!-- end-user-doc -->
+ * 
+ * @generated
+ */
+public final class ArchitecutremodelEditPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	public static final ArchitecutremodelEditPlugin INSTANCE = new ArchitecutremodelEditPlugin();
+
+	/**
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public ArchitecutremodelEditPlugin() {
+		super(new ResourceLocator[] { DatatypesEditPlugin.INSTANCE,
+				EcoreEditPlugin.INSTANCE, ExchangemodelEditPlugin.INSTANCE,
+				ModelEditPlugin.INSTANCE, UnitsEditPlugin.INSTANCE,
+				BasecalculationmodelEditPlugin.INSTANCE, });
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public static class Implementation extends EclipsePlugin {
+		/**
+		 * Creates an instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+		 * 
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
