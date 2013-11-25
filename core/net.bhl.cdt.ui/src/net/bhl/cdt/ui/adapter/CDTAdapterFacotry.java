bcdb613e779a07257feb5aaf572d528d4cd5d625
David Otter <david.otter@bauhaus-luftfahrt.net>
2013-11-25 09:10:30 +0000

    minor fixes
    git-svn-id: https://svn.code.sf.net/p/opencdt/svn/trunk@171 1b14dba9-cd1e-4c95-8206-bb1ba8f2ccbb

--- core/net.bhl.cdt.ui/src/net/bhl/cdt/ui/adapter/CDTAdapterFacotry.java
+++ core/net.bhl.cdt.ui/src/net/bhl/cdt/ui/adapter/CDTAdapterFacotry.java
@@ -3,29 +3,29 @@
  * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  ******************************************************************************/
-/**
- * 
- */
+
 package net.bhl.cdt.ui.adapter;
 
 import org.eclipse.core.runtime.IAdapterFactory;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.IPropertySheetPage;
+import org.eclipse.ui.views.properties.PropertySheet;
 import org.eclipse.ui.views.properties.PropertySheetPage;
 
 /**
- * @author mck
+ * @author mck, David Otter
  */
 public class CDTAdapterFacotry implements IAdapterFactory {
 
 	/**
 	 * Prevent loop in second call.
 	 */
-	private Boolean doubble = false;
+	private Boolean doubble = true;
 	/**
 	 * use the propertySheetPage as a Singleton.
 	 */
 	private static IPropertySheetPage propertySheetPage;
+	private int counter;
 
 	/**
 	 * Returns a adapter of type adapterType for the given object o.
@@ -37,13 +37,15 @@ public class CDTAdapterFacotry implements IAdapterFactory {
 	@SuppressWarnings("rawtypes")
 	public Object getAdapter(Object o, Class adapterType) {
 		if (adapterType == IPropertySheetPage.class) {
-			if (propertySheetPage == null) {
-				propertySheetPage = PropertySheetPageAdapter.getPropertySheetPage();
+			if (counter < 2 || o instanceof PropertySheet) {
+				if (counter++ == 0) {
+					propertySheetPage = PropertySheetPageAdapter.getPropertySheetPage();
+				}
 				return propertySheetPage;
 			}
 			if (doubble) {
-				// Needed to update the PropertyView
-				// TODO: Solution without setFocus
+//				 Needed to update the PropertyView
+//				 TODO: Solution without setFocus?
 				((PropertySheetPage) propertySheetPage).setFocus();
 				((IWorkbenchPart) o).setFocus();
 			}
