5531ef3e4ff0d63c53d49163321815e59d92caf5
David Otter <david.otter@bauhaus-luftfahrt.net>
2013-11-26 12:12:15 +0000

    Commit actual plugin version of Generator
    git-svn-id: https://svn.code.sf.net/p/opencdt/svn/branches/features@187 1b14dba9-cd1e-4c95-8206-bb1ba8f2ccbb

---
+++ matlab/MatlabUpdateSite/net.bhl.generator.matlab/src/net/bhl/generator/matlab/handler/Converter.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * <copyright> Copyright (c) 2009-2013 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ ******************************************************************************/
+package net.bhl.generator.matlab.handler;
+
+import net.bhl.generator.matlab.command.GenerateMatlabClassCommand;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * GenModel to Matlab Converter.
+ * 
+ * @author David Otter
+ * 
+ */
+public class Converter extends AbstractHandler {
+
+	/**
+	 * Get selected Element.
+	 * 
+	 * @param event
+	 *            Selected Element
+	 * @throws ExecutionException
+	 *             Exception
+	 * @return null
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Shell shell = HandlerUtil.getActiveShell(event);
+		ISelection sel = HandlerUtil.getActiveMenuSelection(event);
+		IStructuredSelection selection = (IStructuredSelection) sel;
+
+		Object firstElement = selection.getFirstElement();
+
+		new GenerateMatlabClassCommand().createOutput(shell, firstElement);
+
+		return null;
+	}
+}
