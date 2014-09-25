aec50727a3cb12c6493866516c15c5978495078a
Martin Glas <martin.glas@bauhaus-luftfahrt.net>
2014-09-25 11:21:30 +0200

    Recommitted connector.files and connector.files.edit

---
+++ core/net.bhl.cdt.connector.files/src/net/bhl/cdt/connector/files/FileGenerator.java
@@ -0,0 +1,18 @@
+/**
+ */
+package net.bhl.cdt.connector.files;
+
+import net.bhl.cdt.model.OperationWithInput;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>File Generator</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see net.bhl.cdt.connector.files.FilesPackage#getFileGenerator()
+ * @model abstract="true"
+ * @generated
+ */
+public interface FileGenerator extends FileOperation, OperationWithInput {
+} // FileGenerator
