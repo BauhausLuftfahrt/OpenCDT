b1c7dd5d7a05748dc45c1eb7d142446f52557a2c
rushdi <rushdi@localhost>
2016-11-29 14:02:22 +0100

    CDTConsole Update -Property file -Action Menu -Table View

--- deployment/net.bhl.cdt.client.e4/src/net/bhl/cdt/client/e4/view/CDTConsole.java
+++ deployment/net.bhl.cdt.client.e4/src/net/bhl/cdt/client/e4/view/CDTConsole.java
@@ -1,7 +1,5 @@
 package net.bhl.cdt.client.e4.view;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagLayout;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,7 +7,14 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.text.SimpleDateFormat;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -20,12 +25,20 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.ExpandBar;
+import org.eclipse.swt.widgets.ExpandItem;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.osgi.framework.ServiceReference;
@@ -44,14 +57,127 @@ public class CDTConsole implements LogListener {
 	private static Map<String, String> msgTypeMap;
 	private static Map<String, String> msgTypeIconMap;
 	private List<LogEntry> logList = new ArrayList<LogEntry>();
-	/*private JPanel topActionPanel;
-	private JCheckBox errorCheckbox;
-	private JCheckBox warningCheckbox;
-	private JCheckBox infoCheckbox;
-	private JCheckBox debugCheckbox;*/
+	private List<LogEntry> selectedLogList = new ArrayList<LogEntry>();
+	private static File propertiesFile = new File("msgConfig.properties");
+	private static String errorChecked = "false", infoChecked = "false", warningChecked = "false",
+			debugChecked = "false";
+	private Button errorCheckbox, warningCheckbox, infoCheckbox, debugCheckbox;
+	private int errFlag = 0;
 
 	public CDTConsole() {
 		registerLogViewer();
+
+		if (propertiesFile.exists() && propertiesExist(propertiesFile)) {
+			// System.out.println("Properties file was found and is intact");
+			;
+		} else {
+			// System.out.println("Properties file is being created");
+			createProperties(propertiesFile);
+			// System.out.println("Properties was created!");
+		}
+
+	}
+
+	public static boolean propertiesExist(File propertiesFile) {
+		Properties prop = new Properties();
+		InputStream input = null;
+		boolean exists = false;
+
+		try {
+			input = new FileInputStream(propertiesFile);
+
+			prop.load(input);
+
+			exists = prop.getProperty("errorChecked") != null && prop.getProperty("warningChecked") != null
+					&& prop.getProperty("infoChecked") != null && prop.getProperty("debugChecked") != null;
+
+			if (prop.getProperty("errorChecked") != null && prop.getProperty("warningChecked") != null
+					&& prop.getProperty("infoChecked") != null && prop.getProperty("debugChecked") != null) {
+				errorChecked = prop.getProperty("errorChecked");
+				warningChecked = prop.getProperty("warningChecked");
+				infoChecked = prop.getProperty("infoChecked");
+				debugChecked = prop.getProperty("debugChecked");
+			}
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return exists;
+	}
+
+	public static void createProperties(File propertiesFile) {
+		Properties prop = new Properties();
+		OutputStream output = null;
+
+		try {
+
+			output = new FileOutputStream(propertiesFile);
+
+			prop.setProperty("errorChecked", "false");
+			prop.setProperty("warningChecked", "false");
+			prop.setProperty("infoChecked", "false");
+			prop.setProperty("debugChecked", "false");
+
+			// save properties to project root folder
+			prop.store(output, null);
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	public static void setProperties(String checkedItem, String checkedValue) {
+
+		Properties prop = new Properties();
+		OutputStream output = null;
+
+		try {
+
+			FileInputStream in = new FileInputStream(propertiesFile);
+			prop.load(in);
+			in.close();
+
+			output = new FileOutputStream(propertiesFile);
+
+			if ((!checkedItem.isEmpty() && checkedItem != null) && (!checkedValue.isEmpty() && checkedValue != null)) {
+
+				prop.setProperty(checkedItem, checkedValue);
+				// System.out.println(checkedItem + " " + checkedValue);
+			}
+
+			// save properties to project root folder
+			prop.store(output, null);
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -65,47 +191,81 @@ public class CDTConsole implements LogListener {
 
 	@PostConstruct
 	public void createUI(Composite parent, CDTLogService logger) {
-		parent.setLayout(new GridLayout());
+		propertiesExist(propertiesFile);
+		ExpandBar msgTypeActionBar = new ExpandBar(parent, SWT.NONE);
+		Composite composite = new Composite(msgTypeActionBar, SWT.NONE);
+		GridLayout layout = new GridLayout(6, true);
+		layout.verticalSpacing = 10;
+		composite.setLayout(layout);
 
-		/*
-		 * text = new StyledText(parent, SWT.MULTI | SWT.BORDER | SWT.WRAP |
-		 * SWT.V_SCROLL); text.setLayoutData(new GridData(GridData.FILL_BOTH));
-		 */
-		Composite topActionPanelSWT = new Composite(parent, SWT.FILL);
-		
-		Button errorCheck = new Button(topActionPanelSWT, SWT.CHECK);
-		errorCheck.setText("Hello");
-		
-		/*topActionPanel = new JPanel(parent);
-		errorCheckbox = new CheckBox("Error Log");
-		warningCheckbox = new JCheckBox("Warning Log");
-		infoCheckbox = new JCheckBox("Info Log");
-		debugCheckbox = new JCheckBox("Debug Log");
-		
-		topActionPanel.add(errorCheckbox);
-		topActionPanel.add(warningCheckbox);
-		topActionPanel.add(infoCheckbox);
-		topActionPanel.add(debugCheckbox);
-		topActionPanel.setVisible(true);
+		errorCheckbox = new Button(composite, SWT.CHECK | SWT.NULL);
+		errorCheckbox.setText("Error Log");
 
-		JCheckBox checkbox = new JCheckBox("Enable logging", true);*/
-		
-		logTableViewer = new TableViewer(parent);
+		if (errorChecked.equalsIgnoreCase("true"))
+			errorCheckbox.setSelection(true);
+
+		warningCheckbox = new Button(composite, SWT.CHECK | SWT.NULL);
+		warningCheckbox.setText("Warning Log");
+
+		if (warningChecked.equalsIgnoreCase("true"))
+			warningCheckbox.setSelection(true);
+
+		infoCheckbox = new Button(composite, SWT.CHECK | SWT.NULL);
+		infoCheckbox.setText("Info Log");
+
+		if (infoChecked.equalsIgnoreCase("true"))
+			infoCheckbox.setSelection(true);
+
+		debugCheckbox = new Button(composite, SWT.CHECK | SWT.NULL);
+		debugCheckbox.setText("Debug Log");
+
+		if (debugChecked.equalsIgnoreCase("true"))
+			debugCheckbox.setSelection(true);
+
+		Label label = new Label(composite, SWT.NULL);
+		label.setText("Select Bundle: ");
+		final Combo comboBundle = new Combo(composite, SWT.READ_ONLY);
+		comboBundle.setBounds(50, 50, 150, 65);
+		String bundleItems[] = { "Item One", "Item Two", "Item Three", "Item Four", "Item Five" };
+		comboBundle.setItems(bundleItems);
+
+		ExpandItem items = new ExpandItem(msgTypeActionBar, SWT.NONE, 0);
+		items.setText("Message Types Action");
+		items.setHeight(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		items.setControl(composite); // item0.setImage(image);
+		items.setExpanded(true);
+		msgTypeActionBar.setSpacing(1);
+
+		logTableViewer = new TableViewer(parent, SWT.SINGLE | SWT.BORDER | SWT.FILL | SWT.V_SCROLL | SWT.H_SCROLL);
 		Table table = logTableViewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
+		table.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_END));
 
 		for (int i = 0; i < COLUMN_NAMES.length; i++) {
 			TableColumn tableColumn = new TableColumn(table, SWT.LEFT);
 			tableColumn.setText(COLUMN_NAMES[i]);
 			tableColumn.setWidth(COLUMN_WIDTHS[i]);
+
 		}
 
-		logTableViewer.setContentProvider(new ModelContentProvider());
-		logTableViewer.setLabelProvider(new ModelLabelProvider());
+		table.addListener(SWT.SetData, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		/*
+		 * text = new StyledText(parent, SWT.MULTI | SWT.BORDER | SWT.WRAP |
+		 * SWT.V_SCROLL); text.setLayoutData(new GridData(GridData.FILL_BOTH));
+		 */
 
 		logger.info("CDT Console initialized.");
 		logger.error("This is a sample Error Msg");
+		logger.error("This is a sample Error Msg 1");
+		logger.error("This is a sample Error Msg 2");
 		logger.warning("This is a sample Warning Msg");
 	}
 
@@ -124,32 +284,193 @@ public class CDTConsole implements LogListener {
 
 	@Override
 	public void logged(LogEntry log) {
-
+		logList.add(log);
 		Display.getDefault().asyncExec(new Runnable() {
 
 			public void run() {
 				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-				/*
-				 * //Object newLine = new Date(log.getTime()).toString() + ": "
-				 * + log.getMessage() + " [" + log.getBundle() + "]";
-				 * //logTable.add(newLine); //logTable.reveal(newLine);
-				 * /*text.append( log.getLevel() +"[" +
-				 * log.getBundle().getSymbolicName() + "] " + log.getMessage() +
-				 * " ( " + dateFormat.format(new Date(log.getTime())).toString()
-				 * + " ) " + System.getProperty("line.separator"));
-				 */
+				errorCheckbox.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						Button button = (Button) e.widget;
 
-				logList.add(log);
+						if (button.getSelection()) {
+							setProperties("errorChecked", "true");
+							System.out.println("True Condition");
+							// if (log.getLevel() == 1) {
+							//
+							// if (errFlag == 0) {
+							// selectedLogList.clear();
+							// System.out.println(
+							// "Array Size True : " + logList.size() + " " +
+							// selectedLogList.size());
+							//
+							// run();
+							// errFlag = 1;
+							// }
+							// }
+
+						} else {
+							setProperties("errorChecked", "false");
+
+							System.out.println("False Condition" );
+							//
+							// System.out.println("Array Size false : " +
+							// logList.size() + " " + selectedLogList.size());
+							// if (log.getLevel() == 1) {
+							//
+							// if (errFlag == 1) {
+							// selectedLogList.clear();
+							// run();
+							// errFlag = 0;
+							//
+							// }
+							//
+							// }
+						}
+					}
+				});
+				//
+				// warningCheckbox.addSelectionListener(new SelectionAdapter() {
+				// @Override
+				// public void widgetSelected(SelectionEvent e) {
+				// Button button = (Button) e.widget;
+				// if (button.getSelection()) {
+				// setProperties("warningChecked", "true");
+				// if (log.getLevel() == 2) {
+				// // System.out.println("True Condition");
+				// if (flag == 0) {
+				// // selectedLogList.clear();
+				// run();
+				// }
+				// }
+				// } else {
+				// setProperties("warningChecked", "false");
+				// if (flag == 0) {
+				// // selectedLogList.clear();
+				// System.out
+				// .println("Array Size false : " + logList.size() + " " +
+				// selectedLogList.size());
+				// run();
+				// }
+				// }
+				// }
+				// });
+
+				// infoCheckbox.addSelectionListener(new SelectionAdapter() {
+				// @Override
+				// public void widgetSelected(SelectionEvent e) {
+				// Button button = (Button) e.widget;
+				// if (button.getSelection()) {
+				// setProperties("infoChecked", "true");
+				// if (log.getLevel() == 3) {
+				// if (flag == 0) {
+				// // selectedLogList.clear();
+				// run();
+				// }
+				// }
+				// } else {
+				// setProperties("infoChecked", "false");
+				// if (flag == 0) {
+				// // selectedLogList.clear();
+				// System.out
+				// .println("Array Size false : " + logList.size() + " " +
+				// selectedLogList.size());
+				// run();
+				// }
+				// }
+				// }
+				// });
+				//
+				// debugCheckbox.addSelectionListener(new SelectionAdapter() {
+				// @Override
+				// public void widgetSelected(SelectionEvent e) {
+				// Button button = (Button) e.widget;
+				// if (button.getSelection()) {
+				// setProperties("debugChecked", "true");
+				// if (log.getLevel() == 4) {
+				// if (flag == 0) {
+				// // selectedLogList.clear();
+				// run();
+				// }
+				// }
+				// } else {
+				// setProperties("debugChecked", "false");
+				// if (flag == 0) {
+				// // selectedLogList.clear();
+				// System.out
+				// .println("Array Size false : " + logList.size() + " " +
+				// selectedLogList.size());
+				// run();
+				// }
+				// }
+				// }
+				// });
+
+				selectedLogList.clear();
+
+				if (errorChecked.equalsIgnoreCase("false") && warningChecked.equalsIgnoreCase("false")
+						&& infoChecked.equalsIgnoreCase("false") && debugChecked.equalsIgnoreCase("false")) {
+					System.out.println("Array Size IF : " + logList.size() + " " + selectedLogList.size());
+					// selectedLogList = logList;
+					for (int i = 0; i < logList.size(); i++) {
+						selectedLogList.add(logList.get(i));
+					}
+				} else {
+					for (int i = 0; i < logList.size(); i++) {
+						if (errorChecked.equalsIgnoreCase("true") && logList.get(i).getLevel() == 1) {
+							// System.out.println("err Value : " + i);
+
+							selectedLogList.add(logList.get(i));
+						}
+						if (warningChecked.equalsIgnoreCase("true") && logList.get(i).getLevel() == 2) {
+							// System.out.println("war Value : " + i);
+
+							selectedLogList.add(logList.get(i));
+
+						}
+						if (infoChecked.equalsIgnoreCase("true") && logList.get(i).getLevel() == 3) {
+							// System.out.println("info Value : " + i);
+
+							selectedLogList.add(logList.get(i));
+
+						}
+						if (debugChecked.equalsIgnoreCase("true") && logList.get(i).getLevel() == 4) {
+							// System.out.println("deb Value : " + i);
+
+							selectedLogList.add(logList.get(i));
+
+						}
+
+					}
+				}
+
+				System.out.println("Array Size : " + logList.size() + " " + selectedLogList.size());
 
 				final List<Model> models = new ArrayList<Model>();
 
-				for (int i = 0; i < logList.size(); i++)
-					models.add(new Model(String.valueOf(logList.get(i).getLevel()),
-							String.valueOf(logList.get(i).getLevel()), logList.get(i).getBundle().getSymbolicName(),
-							logList.get(i).getMessage(),
-							dateFormat.format(new Date(logList.get(i).getTime())).toString()));
+				// for (int i = 0; i < logList.size(); i++) {
+				//
+				// models.add(new
+				// Model(String.valueOf(logList.get(i).getLevel()),
+				// String.valueOf(logList.get(i).getLevel()),
+				// logList.get(i).getBundle().getSymbolicName(),
+				// logList.get(i).getMessage(),
+				// dateFormat.format(new
+				// Date(logList.get(i).getTime())).toString()));
+				// }
 
+				for (int i = 0; i < selectedLogList.size(); i++) {
+
+					models.add(new Model(String.valueOf(selectedLogList.get(i).getLevel()),
+							String.valueOf(selectedLogList.get(i).getLevel()),
+							selectedLogList.get(i).getBundle().getSymbolicName(), selectedLogList.get(i).getMessage(),
+							dateFormat.format(new Date(selectedLogList.get(i).getTime())).toString()));
+				}
+
+				logTableViewer.setContentProvider(new ModelContentProvider());
+				logTableViewer.setLabelProvider(new ModelLabelProvider());
 				logTableViewer.setInput(models);
 
 			}
@@ -188,6 +509,7 @@ public class CDTConsole implements LogListener {
 
 		@Override
 		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			// System.out.println(" Hello : " + newInput.toString());
 		}
 
 	}
@@ -199,27 +521,23 @@ public class CDTConsole implements LogListener {
 
 		@Override
 		public Image getColumnImage(Object element, int columnIndex) {
-			// no image to show
-			/*Image image = null;
-			if (!(element instanceof Model)) {
-				return null;
-			}
+			// each element comes from the ContentProvider.getElements(Object)
+			// if (!(element instanceof Model)) {
+			// return null;
+			// }
+			//
+			// Model model = (Model) element;
+			// Image image = null;
+			//
+			// switch (columnIndex) {
+			// case 0:
+			// image = model.getMsgTypeIcon();
+			// return image;
+			// default:
+			// break;
+			//
+			// }
 
-			Model model = (Model) element;
-			Display display = new Display();
-
-			switch (columnIndex) {
-			case 0:
-				image = new Image(display, model.getMsgTypeIcon());
-				GC gc = new GC(image);
-				Rectangle bounds = image.getBounds();
-				gc.drawLine(0, 0, bounds.width, bounds.height);
-				gc.drawLine(0, bounds.height, bounds.width, 0);
-
-				return image;
-			default:
-				break;
-			}*/
 			return null;
 		}
 
@@ -229,6 +547,7 @@ public class CDTConsole implements LogListener {
 			if (!(element instanceof Model)) {
 				return "";
 			}
+
 			Model model = (Model) element;
 
 			switch (columnIndex) {
@@ -265,10 +584,21 @@ public class CDTConsole implements LogListener {
 			super();
 
 			msgTypeIconMap = new HashMap<String, String>();
-			msgTypeIconMap.put("1", "icons/error.PNG");
-			msgTypeIconMap.put("2", "icons/warning.PNG");
-			msgTypeIconMap.put("3", "icons/info.PNG");
-			msgTypeIconMap.put("4", "icons/debug.PNG");
+
+			msgTypeIconMap.put("1", "errorIcon");
+			msgTypeIconMap.put("2", "warningIcon");
+			msgTypeIconMap.put("3", "infoIcon");
+			msgTypeIconMap.put("4", "debugIcon");
+
+			// msgTypeIconMap.put("1", "icons/error.PNG");
+			// msgTypeIconMap.put("2", "icons/warning.PNG");
+			// msgTypeIconMap.put("3", "icons/info.PNG");
+			// msgTypeIconMap.put("4", "icons/debug.PNG");
+
+			// msgTypeIconMap.put("1", "SWT.ICON_ERROR");
+			// msgTypeIconMap.put("2", "SWT.ICON_WARNING");
+			// msgTypeIconMap.put("3", "SWT.ICON_INFORMATION");
+			// msgTypeIconMap.put("4", "SWT.ICON_DEBUG");
 
 			msgTypeMap = new HashMap<String, String>();
 			msgTypeMap.put("1", "Error");
@@ -289,8 +619,10 @@ public class CDTConsole implements LogListener {
 
 			while (itr.hasNext()) {
 				String key = itr.next();
-				if (key.contentEquals(msgTypeIcon))
+				if (key.contentEquals(msgTypeIcon)) {
 					msgTypeIcon = msgTypeIconMap.get(key);
+				}
+
 			}
 
 			return msgTypeIcon;
