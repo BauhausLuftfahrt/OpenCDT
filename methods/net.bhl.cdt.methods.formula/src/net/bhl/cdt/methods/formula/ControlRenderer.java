853d75fef3ddcd1c951f7f295f24d72f22fb5ccb
sanghun.cho <sanghun.cho@bauhaus-luftfahrt>
2017-03-17 15:37:46 +0100

    the image is added below the textfield of latexString

--- methods/net.bhl.cdt.methods.formula/src/net/bhl/cdt/methods/formula/ControlRenderer.java
+++ methods/net.bhl.cdt.methods.formula/src/net/bhl/cdt/methods/formula/ControlRenderer.java
@@ -1,6 +1,10 @@
 package net.bhl.cdt.methods.formula;
 
 import java.awt.Desktop;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 
@@ -32,9 +36,13 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import formula.Formula;
@@ -43,12 +51,12 @@ import formula.FormulaPackage;
 import formula.FormulaRepository;
 import formula.impl.FormulaImpl;
 
+
 public class ControlRenderer extends TextControlSWTRenderer {
 
-	ViewModelContext viewContext;
-	
-	
-
+	private ViewModelContext viewContext;
+	private Label photoLabel;
+	//private Image image;
 
 	@Inject
 	public ControlRenderer(VControl vElement, ViewModelContext viewContext,
@@ -67,9 +75,17 @@ public class ControlRenderer extends TextControlSWTRenderer {
 	@Override
 	protected Control createSWTControl(Composite parent) {
 		
-		
-		
 		final Composite main = new Composite(parent, SWT.NONE);
+		
+		/*this.photoLabel = new Label(main, SWT.DOWN);
+		
+		Image image = new Image(Display.getCurrent(),"C://Users/sanghun.cho/Desktop/stackoverflow.jpg");
+		photoLabel.setImage(image);*/
+		
+		//photoLabel.setLocation(500, 500);
+		//Display display = new Display();
+
+		
 		
 		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(main);
 		
@@ -80,14 +96,60 @@ public class ControlRenderer extends TextControlSWTRenderer {
         
 		final Button button = new Button(main, SWT.PUSH);
 		button.setText("Show"); 
+		this.photoLabel = new Label(main, SWT.DOWN);
+		
+		Image image = new Image(Display.getCurrent(),"C://Users/sanghun.cho/Desktop/stackoverflow.jpg");
+		photoLabel.setImage(image);
 		button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				
+				 /*final File outFile = new File("stackoverflow.jpg");
+				
+				 final MutableLayoutContext params = new LayoutContextImpl(
+		 	    		  LayoutContextImpl.getDefaultLayoutContext());
+		 	     params.setParameter(Parameter.MATHSIZE, 25f);
+		 	      
+		 	     Converter.getInstance().convert(doc, outFile, "image/" + "png", params);
+
+				 byte[] uploadedImg = null;
+		 		 
+			 		try {
+			 			
+			
+			 			double fileLen = outFile.length();
+			 		    uploadedImg = new byte[(int) fileLen];
+			 		    FileInputStream inputStream = new FileInputStream(outFile);
+
+			 		    int nRead = 0;
+			 		    
+			 		    while ((nRead = inputStream.read(uploadedImg)) != -1) {
+			 		    }
+			 		    
+			 		    inputStream.close();
+
+			 		} catch (Exception e2) {
+			 		    // TODO: handle exception
+			 		}
+			 		
+			 		BufferedInputStream inputStreamReader = new BufferedInputStream(new ByteArrayInputStream(uploadedImg));
+			 		ImageData imageData = new ImageData(inputStreamReader);
+			 		Image image = new Image(Display.getCurrent(), imageData);*/
+		 		
+			 		
+			 		//photoLabel.setImage(image);
+				
+				
 			
 				System.out.println("sanghun : "+ viewContext.getDomainModel().eGet(FormulaPackage.Literals.FORMULA__LATEX_STRING));
 
 			}
 		});
+		
+		
+		
+		
+		
 		return control;
 	}
 
