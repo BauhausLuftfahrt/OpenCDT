1da41405565f27137e1493b0cf012be924538554
Michael Sh <michael.shamiyeh@bauhaus-luftfahrt.net>
2017-05-19 15:20:54 +0200

    meta model work;

--- deployment/net.bhl.cdt.client.e4/src/net/bhl/cdt/client/e4/handler/ExitHandler.java
+++ deployment/net.bhl.cdt.client.e4/src/net/bhl/cdt/client/e4/handler/ExitHandler.java
@@ -1,8 +1,6 @@
  
 package net.bhl.cdt.client.e4.handler;
 
-import javax.inject.Inject;
-
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.osgi.service.log.LogService;
 
@@ -10,7 +8,6 @@ import net.bhl.cdt.log.service.CDTLogService;
 
 public class ExitHandler {
 	@Execute
-	@Inject
 	public void execute(CDTLogService log) {
 		log.log(LogService.LOG_INFO, "Shutdown!!!");
 	}	
