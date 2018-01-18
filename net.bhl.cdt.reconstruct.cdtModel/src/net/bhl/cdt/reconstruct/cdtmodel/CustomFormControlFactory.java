3deb3c0f8090238c79d96b1a5e21886415e2a37e
sanghun.cho <sanghun.cho@WMUC0135.bauhaus-luftfahrt.corp>
2018-01-18 16:50:41 +0100

    the input-parameter function should be improved in case of the deleting inputs

--- net.bhl.cdt.reconstruct.cdtModel/src/net/bhl/cdt/reconstruct/cdtmodel/CustomFormControlFactory.java
+++ net.bhl.cdt.reconstruct.cdtModel/src/net/bhl/cdt/reconstruct/cdtmodel/CustomFormControlFactory.java
@@ -491,6 +491,8 @@ public class CustomFormControlFactory extends FormControlFactory {
 		    	 	});
 		    	    listOfHyperlink.add(hyperlink_input);
 	    		}
+	    		
+	    		
 	    		else{
 	    			
 	    			 Hyperlink new_hyperlink_input = _toolkit.createHyperlink(inputParameter_composite, EMPTY ,SWT.NONE);
